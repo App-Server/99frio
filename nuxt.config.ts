34cb29abb37616ece2f64575ad28a8f572c8b179
Aragao <epicappcode@gmail.com>
2025-02-24 19:40:44 -0300

    add-google-tag-manage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,15 @@ export default defineNuxtConfig({
         { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" }
       ],
       script: [
-        { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js", defer: true }
+        { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js", defer: true },
+        {
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-P6T82ZTP');`,
+          type: "text/javascript"
+        }
       ]
     }
   }
