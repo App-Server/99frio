51d669d6f48bd66d0877c6e720da55484a477ae2
Aragao <epicappcode@gmail.com>
2025-03-19 20:30:53 -0300

    add-layout

--- components/Instaladores.vue
+++ components/Instaladores.vue
@@ -4,7 +4,7 @@
             <h3 class="d-flex justify-content-center" style="color: #0059FF;">Encontre Profissionais: Busque por localização ou tipo de serviço.</h3>
             <div class="porqueescolher">
                 <div class="row" style="margin-top: 50px;">
-                    <div class="col-sm-6 mb-3 mb-sm-3">
+                    <div class="col-sm-6 mb-4">
                         <div class="card">
                         <div class="card-body">
                             <h5>Kaique Rodrigo Guilherme Freitas</h5>
@@ -15,20 +15,19 @@
                             <NuxtLink to="/kaique" class="btn btn-info">Contato</NuxtLink>
                         </div>
                         </div>
-                    </div>
-                    <div class="col-sm-6">
+                        <div class="col-sm-6 mb-3">
                         <div class="card">
-                        <div class="card-body">
-                            <h5>Legado Climatização</h5>
-                           <li> <strong> Atende: </strong> Aracaju - SE</li>
-                            <li> <strong> Credenciado: </strong> Gree</li>
-                            <li> <strong> Serviços: </strong>Instalação e manutenção de ar condicionado</li>
-                            <br>
-                            <NuxtLink to="/kaique" class="btn btn-info">Contato</NuxtLink>
+                            <div class="card-body">
+                                <h5>Legado Climatização</h5>
+                                <li> <strong> Atende: </strong> Aracaju - SE</li>
+                                    <li> <strong> Credenciado: </strong> Gree</li>
+                                    <li> <strong> Serviços: </strong>Instalação e manutenção de ar condicionado</li>
+                                    <br>
+                                    <NuxtLink to="/kaique" class="btn btn-info">Contato</NuxtLink>
+                                </div>
+                            </div>                  
                         </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
+                        <div class="col-sm-6 mb-3">
                         <div class="card">
                         <div class="card-body">
                             <h5>Mateus Otávio Edson Bernardes</h5>
@@ -40,7 +39,7 @@
                         </div>
                         </div>
                     </div>
-                    <div class="col-sm-6 mb-sm-3">
+                    <div class="col-sm-6 mb-3">
                         <div class="card">
                         <div class="card-body">
                             <h5>Iago Enzo João Monteiro</h5>
@@ -52,7 +51,7 @@
                         </div>
                         </div>
                     </div>
-                    <div class="col-sm-6">
+                    <div class="col-sm-6 mb-3">
                         <div class="card">
                         <div class="card-body">
                             <h5>Noah Nelson Gabriel Caldeira</h5>
@@ -64,7 +63,7 @@
                         </div>
                         </div>
                     </div>
-                    <div class="col-sm-6 mb-sm-3">
+                    <div class="col-sm-6 mb-3">
                         <div class="card">
                         <div class="card-body">
                             <h5>Rodrigo Kaique Rezende</h5>
@@ -76,7 +75,7 @@
                         </div>
                         </div>
                     </div>
-                    <div class="col-sm-6">
+                    <div class="col-sm-6 mb-3">
                         <div class="card">
                         <div class="card-body">
                             <h5>Benício Gael Carlos Eduardo Galvão</h5>
@@ -88,7 +87,7 @@
                         </div>
                         </div>
                     </div>
-                    <div class="col-sm-6 mb-sm-3">
+                    <div class="col-sm-6 mb-3">
                         <div class="card">
                         <div class="card-body">
                             <h5>Heitor Matheus Vinicius Bernardes</h5>
@@ -100,7 +99,7 @@
                         </div>
                         </div>
                     </div>
-                    <div class="col-sm-6">
+                    <div class="col-sm-6 mb-3">
                         <div class="card">
                         <div class="card-body">
                             <h5>Guilherme Ricardo Duarte</h5>
@@ -112,7 +111,7 @@
                         </div>
                         </div>
                     </div>
-                    <div class="col-sm-6 mb-sm-3">
+                    <div class="col-sm-6 mb-3">
                         <div class="card">
                         <div class="card-body">
                             <h5>Joaquim Sebastião Edson Brito</h5>
@@ -124,7 +123,7 @@
                         </div>
                         </div>
                     </div>
-                    <div class="col-sm-6">
+                    <div class="col-sm-6 mb-3">
                         <div class="card">
                         <div class="card-body">
                             <h5>Julio Henrique da Conceição</h5>
@@ -136,7 +135,7 @@
                         </div>
                         </div>
                     </div>
-                    <div class="col-sm-6 mb-sm-3">
+                    <div class="col-sm-6 mb-3">
                         <div class="card">
                         <div class="card-body">
                             <h5>Caio Noah Aparício</h5>
@@ -148,19 +147,19 @@
                         </div>
                         </div>
                     </div>
-                    <div class="col-sm-6">
+                    <div class="col-sm-6 mb-3">
                         <div class="card">
-                        <div class="card-body">
-                            <h5>Osvaldo Oliver Barros</h5>
-                           <li> <strong> Atende: </strong> Aracaju - SE</li>
-                            <li> <strong> Credenciado: </strong> Gree</li>
-                            <li> <strong> Serviços: </strong>Instalação e manutenção de ar condicionado</li>
-                            <br>
-                            <NuxtLink to="/kaique" class="btn btn-info">Contato</NuxtLink>
-                        </div>
-                        </div>
+                            <div class="card-body">
+                                <h5>Osvaldo Oliver Barros</h5>
+                                <li> <strong> Atende: </strong> Aracaju - SE</li>
+                                    <li> <strong> Credenciado: </strong> Gree</li>
+                                    <li> <strong> Serviços: </strong>Instalação e manutenção de ar condicionado</li>
+                                    <br>
+                                    <NuxtLink to="/kaique" class="btn btn-info">Contato</NuxtLink>
+                            </div>
+                            </div>
                     </div>
-                    <div class="col-sm-6">
+                    <div class="col-sm-6 mb-3">
                         <div class="card">
                         <div class="card-body">
                             <h5>Lucca Mateus Kevin Novaes</h5>
@@ -172,6 +171,9 @@
                         </div>
                         </div>
                     </div>
+                    
+                    </div>
+                    
                 </div>
             </div>
         </div>
